3ab507e5516851d4b72db44ebb5af314c85b70f1
basiten <rahmgerr.bbss@gmail.com>
2018-12-23 02:19:07 +0900

    砲身を指定できるように修正

--- Assets/Scripts/CannonBase.cs
+++ Assets/Scripts/CannonBase.cs
@@ -12,9 +12,12 @@ public class CannonBase : MonoBehaviour
 
     // List<Collider> m_collider_list;
     HashSet<GameObject> m_in_range_object_list = new HashSet<GameObject>();
+    GameObject m_barrel;
 
     void Start()
     {
+        m_barrel = transform.gameObject;
+        // m_barrel = transform.Find("Barrel").gameObject;
     }
 
 
@@ -67,11 +70,13 @@ public class CannonBase : MonoBehaviour
     //      void
     void AimTo(Vector3 target)
     {
-        var aim = new Vector3(target.x, transform.position.y, target.z);
-        var angle_dif = Vector3.Angle(transform.forward, aim);
-        var angle_move = (angle_dif < maxRotateVelocity) ? angle_dif : maxRotateVelocity;
-        var rotate_dir = Vector3.Cross(transform.forward, aim).normalized * angle_move;
-        transform.Rotate(rotate_dir);
+        var aim = new Vector3(target.x, m_barrel.transform.position.y, target.z);
+        var angle_dif = Vector3.Angle(m_barrel.transform.forward, aim);
+        var angle_move = (angle_dif < maxRotateVelocity * Time.deltaTime) ? angle_dif : maxRotateVelocity * Time.deltaTime;
+        var rotate_vector = Vector3.Cross(m_barrel.transform.forward, aim).normalized * angle_move;
+        m_barrel.transform.Rotate(rotate_vector);
+        // m_barrel.transform.localPosition = Vector3.Dot(rotate_vector, m_barrel.transform.localPosition);
+        m_barrel.transform.localPosition = Quaternion.AngleAxis(angle_move, rotate_vector.normalized) * m_barrel.transform.localPosition;
 
     }
 
