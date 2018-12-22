625ec3b76fb9ccb341a6ec3ce97d4201cf483a6c
Hiromu-S <homuhomu0131@gmail.com>
2018-12-22 17:27:20 +0900

    network test

---
+++ Assets/Scripts/NetworkPlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkPlayerController : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer == true)
+        {
+            Move();
+        }  
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * 0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.forward * -0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += transform.right * -0.2f;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * 0.2f;
+        }
+    }
+}
---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class main : NetworkBehaviour
+{
+
+    public GameObject Cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 300 == 0)
+        {
+            CmdCreateCube();
+        }
+    }
+
+    void CmdCreateCube()
+    {
+        GameObject obj = (GameObject)Instantiate(Cube, transform.position, transform.rotation);
+        NetworkServer.Spawn(obj);
+    }
+}
