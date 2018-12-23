d124989f1f505c62b909e35630a2da0c87298328
basiten <rahmgerr.bbss@gmail.com>
2018-12-23 12:43:34 +0900

    cannonのaim targetが原点じゃないと正常に動作しなかったのを修正

--- Assets/Scripts/CannonBase.cs
+++ Assets/Scripts/CannonBase.cs
@@ -12,12 +12,12 @@ public class CannonBase : Entity
 
     // List<Collider> m_collider_list;
     HashSet<GameObject> m_in_range_object_list = new HashSet<GameObject>();
-    GameObject m_barrel;
+    [SerializeField] GameObject m_barrel;
 
     public new void Start()
     {
         base.Start();
-        m_barrel = transform.gameObject;
+        // m_barrel = transform.gameObject;
         // m_barrel = transform.Find("Barrel").gameObject;
     }
 
@@ -71,13 +71,12 @@ public class CannonBase : Entity
     //      void
     void AimTo(Vector3 target)
     {
-        var aim = new Vector3(target.x, m_barrel.transform.position.y, target.z);
+        var pos = transform.position;
+        var aim = new Vector3(target.x - pos.x, 0, target.z - pos.z);
         var angle_dif = Vector3.Angle(m_barrel.transform.forward, aim);
         var angle_move = (angle_dif < maxRotateVelocity * Time.deltaTime) ? angle_dif : maxRotateVelocity * Time.deltaTime;
         var rotate_vector = Vector3.Cross(m_barrel.transform.forward, aim).normalized * angle_move;
         m_barrel.transform.Rotate(rotate_vector);
-        // m_barrel.transform.localPosition = Vector3.Dot(rotate_vector, m_barrel.transform.localPosition);
-        m_barrel.transform.localPosition = Quaternion.AngleAxis(angle_move, rotate_vector.normalized) * m_barrel.transform.localPosition;
 
     }
 
